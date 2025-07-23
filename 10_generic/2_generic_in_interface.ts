d405feb0fd4f73cf823af70339429afa5ad9a5fa
제원모 <wpdnjsah@icloud.com>
2025-07-23 22:06:06 +0900

    커밋

--- 10_generic/2_generic_in_interface.ts
+++ 10_generic/2_generic_in_interface.ts
@@ -2,22 +2,22 @@
  * Generic in Interface
  */
 interface Cache<T> {
-    data: T[];
-    lastUpdate: Date;
+  data: T[];
+  lastUpdate: Date;
 }
 
 const cache1: Cache<string> = {
-    data: ['data1', 'data2'],
-    lastUpdate: new Date(),
-}
+  data: ['data1', 'data2'],
+  lastUpdate: new Date(),
+};
 
 // const cach2: Cache<number> = {
 //     data: [123, 456],
 //     lastUpdate: new Date(),
 // }
 
-interface DefaultGeneric<T = string>{
-    data:T[];
+interface DefaultGeneric<T = string> {
+  data: T[];
 }
 
 // const cache3: DefaultGeneric = {
